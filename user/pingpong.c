c24f7ddddaa662487abbfec4bacf953603c2bf03
ChenQiqian <cqqqwq@outlook.com>
2021-12-02 16:27:01 +0800

    add repeat test

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,8 +2,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {
-    int ticks_before = uptime();
+void test_once(){
     int p_1[2], p_2[2];
     char a[2]; // read and write buffers
     pipe(p_1);
@@ -16,7 +15,7 @@ int main(int argc, char *argv[]) {
         read(p_1[0], a, 1);
         close(p_1[1]);
         close(p_1[0]);
-        printf("p_1:%d %d\n",p_1[0],p_1[1]);
+        // printf("p_1:%d %d\n",p_1[0],p_1[1]);
         write(p_2[1], a, 1);
         close(p_2[1]);
         exit(0);
@@ -25,8 +24,15 @@ int main(int argc, char *argv[]) {
     read(p_2[0], a, 1);
     close(p_2[1]);
     close(p_2[0]);
-    printf("finished\n");
+    // printf("finished\n");
     wait(0);
+}
+
+int main(int argc, char *argv[]) {
+    int ticks_before = uptime();
+    for(int i = 0;i < 100000;i++){
+        test_once();
+    }
     int ticks_after = uptime();
     int ticks_diff = ticks_after - ticks_before;
     printf("%d\n", ticks_diff);
